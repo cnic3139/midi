5fd1514e0783f07faaef022067cd4402f4ab8551
Cameron Nichols <cnic3139@uni.sydney.edu.au>
2018-09-08 13:58:28 +1000

    Added events throughout player for more functionality. Added promise to signal end of playing, begun implementing dequeue() method which, at present, is responsible for sending midi notes. Made notes at bottom of file about further work, i.e. the ability to change sequences based on input from the midi machine itself.

--- Player.ts
+++ Player.ts
@@ -1,24 +1,34 @@
 import { Midi } from './midi'
-import { Note } from './types';
+import { Instrument, Note } from './types';
 import { EventEmitter } from 'events';
-import { EVENT_QUEUE_NONEMPTY } from './constants';
+import { PLAYER_START, PLAYER_STOP, EVENT_QUEUE_EMPTY, EVENT_QUEUE_NONEMPTY } from './constants';
+import { Promise } from 'es6-promise';
+import { Util } from './util';
 
 export class Player {
 
 	private queue: Note[] = [];
 	private playing: boolean = false;
-	private queueNonEmpty: EventEmitter = new EventEmitter();
+	private queueEventEmitter: EventEmitter = new EventEmitter();
+	private instrument: Instrument;
 
 	constructor(private readonly midi) {}
 
 	// Start the Player dequeueing notes and playing them
-	public begin(): void {
+	public begin(finish: boolean = false): void {
+		this.queueEventEmitter.emit(PLAYER_START);
 		this.playing = true;
-		this.dequeue;
+		if (finish) {
+			this.queueEventEmitter.on(EVENT_QUEUE_EMPTY, () => {
+				this.stop();
+			});
+		}
+		this.dequeue();
 	}
 
 	// Stop the Player dequeueing notes and playing them.
 	public stop(): void {
+		this.queueEventEmitter.emit(PLAYER_STOP);
 		this.playing = false;
 	}
 
@@ -26,8 +36,16 @@ export class Player {
 	public play(note: Note): void {
 		this.queue.push(note);
 		if (this.queue.length === 1) {
-			this.queueNonEmpty.emit(EVENT_QUEUE_NONEMPTY);
+			this.queueEventEmitter.emit(EVENT_QUEUE_NONEMPTY);
 		}
+	}
+
+	public awaitFinish(): Promise<void> {
+		return new Promise((resolve) => {
+			this.queueEventEmitter.on(PLAYER_STOP, () => {
+				resolve();
+			});
+		});
 	}
 
 	// Private method responsible for dequeueing notes.
@@ -36,17 +54,32 @@ export class Player {
 	private dequeue(): void {
 		if (this.playing) {
 			if (!this.queue.length) {
-				this.queueNonEmpty.on(EVENT_QUEUE_NONEMPTY, this.dequeue);
+				this.queueEventEmitter.emit(EVENT_QUEUE_EMPTY);
+				this.queueEventEmitter.on(EVENT_QUEUE_NONEMPTY, this.dequeue);
 			} else {
-				const noteToPlay = this.queue.shift();
+				const note: Note = this.queue.shift();
 				// Actually play the note.
-				
+				this.midi.send({
+					status: 144,
+					data1: Util.noteToNum(note),
+					data2: 127
+				});
 				// Wait duration of note, then send note off message?
+				setTimeout(() => this.midi.send({
+					status: 128,
+					data1: Util.noteToNum(note),
+					data2: 64
+				}), note.duration);
 
 				// So we need a note duration time and a 
 				// 	between-note duration time.
-				setTimeout(this.dequeue, noteToPlay.duration);
+				setTimeout(() => {this.dequeue()}, note.duration + 100/*note.duration*/);
 			}
 		}
 	}
+
+	// I want to make it so that incoming midi notes from instruments can
+	// dynamically change what's happening.
+	// i.e if a Player is playing a Loop, i want to be able to change the 
+	// tonic note of the loop by pressing keys on the synth, for example.
 }
